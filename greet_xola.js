21af44754644c7e8e6d7cc00e00c3752cc2bb5de
namhla1221 <namhlamthi21@gmail.com>
2020-08-04 12:12:04 +0200

    new changes

--- greet_xola.js
+++ greet_xola.js
@@ -4,6 +4,18 @@
 const chalk = require('chalk');
 //import the greet module that is in the current folder
 const greet = require('./greet');
+const figlet = require('figlet');
 
-const styledMessage = chalk.bgGreen.black(greet('Xola'));
-console.log(styledMessage)
+const greeting = greet('xola');
+
+figlet(greeting, function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    // console.log(data)
+
+    const styledMessage = chalk.bgGreen.black(data);
+    console.log(styledMessage)
+});
